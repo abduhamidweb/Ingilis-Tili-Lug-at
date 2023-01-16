a9d28060ac570df9bf9ac917d0d0368e7ce20538
Abduhamid <abduhamidbotirovweb@gmail.com>
2023-01-16 21:43:38 +0500

    Words Container add

---
+++ src/Components/WordsContainer/WordsContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./index.scss"
+const WordsContainer = () => {
+    return (
+        <>
+            <div className="words__wrap">
+
+            </div>
+        </>
+    );
+};
+
+export default WordsContainer;
--- src/Routers/index.jsx
+++ src/Routers/index.jsx
@@ -4,11 +4,13 @@ import Submit from '../Components/Submit/Submit'
 import Challenge from '../Components/Challenge/Challenge'
 import Home from '../pages/Home/Home'
 import BookPage from '../Components/bookPage/BookPage'
+import WordsContainer from '../Components/WordsContainer/WordsContainer'
 
 const index = () => {
   return (
     <>
       <Routes>
+        {/* <Route path='/' element={<WordsContainer />} /> */}
         <Route path='/' element={<Home />} />
         <Route path='/home' element={<Home />} />
         <Route path='/home/:id' element={<BookPage />} />
